ab8503f11de99b030368d24faf7c8788de9eb84e
Florian Dold <florian.dold@gmail.com>
2019-10-14 22:03:24 +0530

    build scripts as library

---
+++ testconfigure.py
@@ -0,0 +1,13 @@
+from talerbuildconfig import *
+
+b = BuildConfig()
+b.enable_prefix()
+b.enable_configmk()
+b.add_tool(YarnTool())
+b.add_tool(BrowserTool())
+b.add_tool(PyBabelTool())
+b.add_tool(NodeJsTool())
+b.add_tool(PosixTool("find"))
+b.add_tool(PosixTool("xargs"))
+b.add_tool(PosixTool("msgmerge"))
+b.run()
